edb8e41306f5d77586f50ae8ed61d61d85895852
user1 <vickythebest008@gmail.com>
2019-12-30 14:54:59 -0800

    test commit from VSC

--- test_solution_1.py
+++ test_solution_1.py
@@ -1,9 +1,11 @@
+# TEST commit from VSC
 from solution_1 import solution
 
+
 def test_solution():
-    list_in=[10,15,3,7]
-    k=17
-    expected_out =True
+    list_in = [10, 15, 3, 7]
+    k = 17
+    expected_out = True
 
     result = solution(list_in, k)
 
